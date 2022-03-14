0817ee51fa60e09357d89945045abe11bf9cd58f
Emily Grace Lester <emhagerman5678@gmail.com>
2022-03-14 12:55:54 -0500

    Redoing all exercises

---
+++ Exercise 1.js
@@ -0,0 +1,6 @@
+// Aim: Create a new project (with only an HTML file) that uses JavaScript to change the color of a Heading element.
+
+// Create new folder with HTML file
+ //Add a Heading Element with text inside
+ // Create a <script></script> tag at the bottom of the page
+ // Write the logic to select a heading and change its color
