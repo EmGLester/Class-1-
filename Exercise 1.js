22f41227c6915725282f532ec9423db6861779c4
Emily Grace Lester <emhagerman5678@gmail.com>
2022-03-14 14:05:12 -0500

    finished with basic

--- Exercise 1.js
+++ Exercise 1.js
@@ -11,23 +11,14 @@
 // of a Heading element on a button click.
 
 
-// Selects the first "h1" tag in the document
-const myHeading = document.querySelector("h1");
+// Change the body's background color, and the text color on click
 
-// Selects the first <h1> element
 const headingOne = document.querySelector("h1");
-
-// Selects the body element
 const bodyEl = document.querySelector("body");
 
-// Changes the "headingOne" variable's text content
-headingOne.innerText = "Updated Hello World From JavaScript!";
 
-// Changes the "headingTwo" variable's text color
-headingTwo.style.color = "#134e4a";
+function Updatecolor () {
+    bodyEl.style.backgroundColor = "#0c4a6e";
+    headingOne.style.color = "#f0f9ff";  
+}
 
-// Change the body's background color, and the text color on click
-function updateColors() {
-  bodyEl.style.backgroundColor = "#0c4a6e";
-  headingOne.style.color = "#f0f9ff";
-  headingTwo.style.color = "#7dd3fc";
