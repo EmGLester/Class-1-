6d62fcd5631a80ac0e1580e5f9076615d86b8aa1
Emily Grace Lester <emhagerman5678@gmail.com>
2022-03-17 14:48:54 -0500

    ts updates

---
+++ helloworld.ts
@@ -0,0 +1,18 @@
+// Exercise 1 // 
+
+// Aim: Create a new project (with only an HTML file) that uses JavaScript to change the color of a Heading element.
+
+// Create new folder with HTML file
+ //Add a Heading Element with text inside
+ // Create a <script></script> tag at the bottom of the page
+ // Write the logic to select a heading and change its color
+
+
+
+// Aim: Create a new project (with an HTML and JavaScript) that uses JavaScript to change the color 
+// of a Heading element on a button click.
+
+
+// Change the body's background color, and the text color on click
+
+
